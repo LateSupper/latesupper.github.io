36423e290f895bc109bf7e610a5d4ff7f677d1db
LateSupper <46272253+LateSupper@users.noreply.github.com>
2019-03-10 16:58:11 -0500

    Add emailCopy script

---
+++ js/emailCopy.js
@@ -0,0 +1,5 @@
+// prompts visitor to manually copy email address for emailing officiallatesupper@gmail.com
+
+function copyEmail(text) {
+    window.prompt("Copy to clipboard, hit OK to continue", text);
+}
